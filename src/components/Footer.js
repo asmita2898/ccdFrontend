2922b32ac41a9dc82113b9995dc65bc0c7f74815
Asmita Bhinge <aasmitabhinge@gmail.com>
2023-03-15 12:07:08 +0530

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -69,7 +69,7 @@ const Footer = () => {
         </div>
 
         <div className="text-center p-3" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
-          © 2023 Copyright:
+          © 2023 Copyright
           <a className="text-white text-decoration-none" href='wwww.ccd.com'> cafedaydreamer.com</a>
         </div>
 
